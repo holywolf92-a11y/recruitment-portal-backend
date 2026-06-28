661deb7b720965ad0c0c555c18c7033fb855cf22
holywolf92-a11y <holywolf92@gmail.com>
2026-06-28 20:24:43 +0500

    Revert "feat(facebook): add Facebook Page posting for job leads"
    This reverts commit 5855cb4fb6bb58c1af527e4d9afe3ff53f9aab2b.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -32,7 +32,6 @@ import databankRoutes from './databank';
 import jobLeadsRoutes from './jobLeads';
 import apiTokensRoutes from './apiTokens';
 import extensionRoutes from './extension';
-import facebookRoutes from './facebook';
 import { getWorkerStatus } from '../controllers/workerStatusController';
 
 const router = Router();
@@ -70,7 +69,6 @@ router.use('/databank', databankRoutes);
 router.use('/jobs', jobLeadsRoutes);
 router.use('/auth/tokens', apiTokensRoutes);
 router.use('/extension', extensionRoutes);
-router.use('/facebook', facebookRoutes);
 router.get('/worker-status', getWorkerStatus);
 
 export default router;
